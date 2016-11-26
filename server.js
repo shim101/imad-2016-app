4e866c162936d0ffca75d0e0440339be0b5032a6
shim101 <shaileshkumar220@gmail.com>
2016-11-26 12:03:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
